33800e263add9e21f2a1d77415f1a7e1bbfb9465
Sergei Poliakov <white.irbys@gmail.com>
2020-03-09 17:24:26 +0200

    Remove README.md, amend .gitignore, add common cmake files

---
+++ cmake/bootstrap.cmake
@@ -0,0 +1,7 @@
+include(${CMAKE_CURRENT_LIST_DIR}/options.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/ccache.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/gcc.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/gold.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/sanitizers.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/warnings.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/werror.cmake)
---
+++ cmake/sanitizers.cmake
@@ -0,0 +1,30 @@
+if(ADDRESS_SANITIZER)
+   # https://clang.llvm.org/docs/AddressSanitizer.html
+   message(STATUS "Enabled address sanitizer")
+   set(ASAN_FLAGS "-fsanitize=address -fsanitize-recover=address")
+   set(ASAN_ADDITIONAL_FLAGS "-fno-omit-frame-pointer -fno-optimize-sibling-calls")
+   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ASAN_FLAGS} ${ASAN_ADDITIONAL_FLAGS}" PARENT_SCOPE)
+   set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${ASAN_FLAGS}" PARENT_SCOPE)
+endif()
+
+if(UNDEFINED_SANITIZER)
+   message(STATUS "Enabled undefined behavior sanitizer")
+   set(UBSAN_FLAGS "-fsanitize=undefined -fsanitize-recover=undefined")
+   set(UBSAN_ADDITIONAL_FLAGS "-fno-omit-frame-pointer")
+   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${UBSAN_FLAGS} ${UBSAN_ADDITIONAL_FLAGS}" PARENT_SCOPE)
+   set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${UBSAN_FLAGS}" PARENT_SCOPE)
+endif()
+
+if(THREAD_SANITIZER)
+   # https://clang.llvm.org/docs/ThreadSanitizer.html
+   message(STATUS "Enabled thread sanitizer")
+   set(TSAN_FLAGS "-fsanitize=thread")
+   set(TSAN_ADDITIONAL_FLAGS "-fno-omit-frame-pointer")
+   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ASAN_FLAGS} ${TSAN_ADDITIONAL_FLAGS}" PARENT_SCOPE)
+   set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${TSAN_FLAGS}" PARENT_SCOPE)
+endif()
+
+if(MEMORY_SANITIZER)
+   # https://clang.llvm.org/docs/MemorySanitizer.html
+   set(MSAN_ADDITIONAL_FLAGS "-fno-omit-frame-pointer -fno-optimize-sibling-calls")
+endif()
